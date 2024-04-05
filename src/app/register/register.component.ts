e2dce4a3fa17d59cc3a9420939e33cb06b06ea6b
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-05 17:28:22 +0530

    updating...

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
+  imports: [FormsModule],
 })
 export class RegisterComponent {
 
+  private apiUrl = 'http://localhost:8000';
+
+  constructor(private http: HttpClient) { }
+
+  registerForm = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    mobile: '',
+    email: '',
+    password: '',
+    confirmPassword: '' // Added confirmPassword field
+  };
+
+  onSubmit(formData: any) {
+    // Send registration data to backend
+    this.http.post<any>(`${this.apiUrl}/register`, formData)
+      .subscribe(
+        (response: any) => {
+          console.log('Registration successful:', response);
+          // Handle successful registration (e.g., show success message)
+        },
+        (error: any) => {
+          console.error('Registration error:', error);
+          // Handle registration error (e.g., display error message to user)
+        }
+      );
+  }
 }
