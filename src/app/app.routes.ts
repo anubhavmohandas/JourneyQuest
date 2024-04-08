5419dbe996b6510009cb5e44da982347be068148
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-08 15:40:12 +0530

    routing wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: RegisterComponent},
+    {path:'login', component: LoginComponent},
+];
