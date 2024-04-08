a72be97657ea12910261ef3ee778355e6caef302
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-08 16:19:41 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { BucketlistComponent } from './bucketlist/bucketlist.component';
 
 export const routes: Routes = [
     {path:'', component: RegisterComponent},
