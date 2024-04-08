e8227d810e000917fcc4bc06ae9642cfd648c7ab
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-08 16:02:49 +0530

    home page wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BucketlistComponent } from './bucketlist/bucketlist.component';
 
 export const routes: Routes = [
     {path:'', component: RegisterComponent},
+    // {path:'register', component: RegisterComponent},
     {path:'login', component: LoginComponent},
+    {path:'dashboard', component: DashboardComponent}
 ];
