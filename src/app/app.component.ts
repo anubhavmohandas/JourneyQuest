d3dcb240edf34fe67d617c3256e5a7b033a5ff4a
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-16 01:05:05 +0530

    contact us done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactUsComponent } from './contactus/contactus.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, AboutusComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, AboutusComponent, ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.css'
 })
-export class ContactusComponent {
+export class ContactUsComponent {
+  message = {
+    name: '',
+    email: '',
+    subject: '',
+    content: ''
+  };
 
+  onSubmit() {
+   alert('Form submitted:');
+
+   this.clearForm();
+  }
+
+  clearForm() {
+    this.message = {
+      name: '',
+      email: '',
+      subject: '',
+      content: ''
+    };
+  }
 }
