bf6ae2160329efc9dde11e3ca44066df107d72b7
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-16 00:17:06 +0530

    wip about us

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './aboutus.component.css'
 })
 export class AboutusComponent {
+  teamMembers = [
+    {
+      name: 'Anubhav Mohandas',
+      role: 'Founder',
+      bio: 'A passionate entrepreneur of the tech industry.'
+    },
+    {
+      name: 'Saniya Shaikh',
+      role: 'Co - Founder',
+      bio: 'A skilled developer who loves creating innovative solutions.'
+    }
+  ];
 
+  mission = 'Our mission is to provide high-quality products and services to our customers, ensuring customer satisfaction and innovation in everything we do.';
+
+  vision = 'Our vision is to become a leading company in the tech industry, recognized for our commitment to excellence, innovation, and customer satisfaction.';
+
+  constructor() { }
 }
