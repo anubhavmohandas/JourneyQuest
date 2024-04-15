88d425f797fe84d00af99fb4ad16e14b0ed21415
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-16 01:16:12 +0530

    bl updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactUsComponent } from './contactus/contactus.component';
+import { BucketListComponent } from './bucketlist/bucketlist.component';
+import { TravelComponent } from './travel/travel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, AboutusComponent, ContactUsComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, AboutusComponent, ContactUsComponent, BucketListComponent, TravelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/bucketlist/bucketlist.component.ts
+++ src/app/bucketlist/bucketlist.component.ts
@@ -1,12 +1,51 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export interface BucketListItem {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  url?: string;
+  isCompleted: boolean;
+}
 
 @Component({
   selector: 'app-bucketlist',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './bucketlist.component.html',
   styleUrl: './bucketlist.component.css'
 })
-export class BucketlistComponent {
+export class BucketListComponent {
+  bucketList: BucketListItem[] = [];
+  newItem: BucketListItem = {
+    id: 0,
+    title: '',
+    description: '',
+    category: '',
+    isCompleted: false
+  };
 
+  addItem() {
+    // Generate a unique ID for the new item
+    this.newItem.id = Date.now();
+    
+    // Add the new item to the bucket list
+    this.bucketList.push(this.newItem);
+
+    // Clear the form
+    this.newItem = {
+      id: 0,
+      title: '',  
+      description: '',
+      category: '',
+      isCompleted: false
+    };
+  }
+
+  deleteItem(id: number) {
+    this.bucketList = this.bucketList.filter(item => item.id !== id);
+  }
 }
