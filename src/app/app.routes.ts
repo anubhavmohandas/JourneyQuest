53f297b9bef417e80f3a36e4e41abbe43b244d68
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-16 07:49:44 +0530

    work in progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactUsComponent } from './contactus/contactus.component';
 
 export const routes: Routes = [
     {path:'', component: RegisterComponent},
     {path:'register', component: RegisterComponent},
     {path:'login', component: LoginComponent},
-    {path:'dashboard', component: DashboardComponent}
+    {path:'dashboard', component: DashboardComponent},
+    {path:'aboutus', component: AboutusComponent}, 
+    {path:'contactus', component: ContactUsComponent}
 
 ];
--- src/app/travel/travel.component.ts
+++ src/app/travel/travel.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-travel',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './travel.component.html',
   styleUrl: './travel.component.css'
 })
 export class TravelComponent {
+  newTrip = {
+    tripName: '',
+    tripDescription: '',
+    destination: '',
+    country: '',
+    city: '',
+    startDate: '',
+    endDate: '',
+    transportation: '',
+    departureCity: '',
+    departureDate: '',
+    returnDate: '',
+    accommodationType: '',
+    accommodationName: '',
+    bookingConfirmation: '',
+    activities: '',
+    attractions: '',
+    totalBudget: '',
+    dailyBudget: '',
+    notes: '',
+    packingList: ''
+  };
 
+  planTrip() {
+    // Logic to handle trip planning
+    console.log('Planning trip:', this.newTrip);
+    
+    // Clear the form after submission
+    this.clearForm();
+  }
+
+  clearForm() {
+    this.newTrip = {
+      tripName: '',
+      tripDescription: '',
+      destination: '',
+      country: '',
+      city: '',
+      startDate: '',
+      endDate: '',
+      transportation: '',
+      departureCity: '',
+      departureDate: '',
+      returnDate: '',
+      accommodationType: '',
+      accommodationName: '',
+      bookingConfirmation: '',
+      activities: '',
+      attractions: '',
+      totalBudget: '',
+      dailyBudget: '',
+      notes: '',
+      packingList: ''
+    };
+  }
 }
