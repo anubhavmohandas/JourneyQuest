3c4b4b8ee9d5e9a8d34df64374ae87bf4be47f1c
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-18 02:31:43 +0530

    working on

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
+  currentSlide = 0;
+
+  ngOnInit(): void {
+    this.showSlides(this.currentSlide);
+  }
+
+  plusSlides(n: number): void {
+    this.showSlides(this.currentSlide += n);
+  }
+
+  currentSlideIndex(n: number): void {
+    this.showSlides(this.currentSlide = n);
+  }
+
+  showSlides(n: number): void {
+    let i;
+    const slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    const dots = document.getElementsByClassName("dot") as HTMLCollectionOf<HTMLElement>;
+
+    if (n > slides.length - 1) { this.currentSlide = 0; }
+    if (n < 0) { this.currentSlide = slides.length - 1; }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[this.currentSlide].style.display = "block";
+    dots[this.currentSlide].className += " active";
+  }
 }
