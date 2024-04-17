fd944158b9e09d3d0a90f7fbe2e0062f2db734b0
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-17 13:37:55 +0530

    updating

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  // imports: [Router],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // Add properties for username and password if needed
+  username: string = '';
+  password: string = '';
 
+  // constructor(private router: Router) { }
+
+  onSubmit() {
+    // Implement login logic here
+    // This example just navigates to another route for demonstration
+    //this.router.navigate(['/dashboard']); // Replace with your desired route after successful login
+  }
 }
