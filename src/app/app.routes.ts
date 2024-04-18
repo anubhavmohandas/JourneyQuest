81e10c758d4d2c3813d5bbd37f76abdd821a5a2a
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-18 08:35:59 +0530

    70% done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { ExtraOptions, RouterLink, RouterLinkActive, RouterModule, RouterOutlet, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -6,15 +6,28 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactUsComponent } from './contactus/contactus.component';
 import { BucketListComponent } from './bucketlist/bucketlist.component';
 import { TravelComponent } from './travel/travel.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const routerOptions: ExtraOptions = {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    scrollOffset: [0, 64],
+};
 
 export const routes: Routes = [
     // {path:'', component: RegisterComponent},
     {path:'register', component: RegisterComponent},
     {path:'login', component: LoginComponent},
+    {path:'bucketlist', component: BucketListComponent},
+    {path:'travel', component: TravelComponent},
     {path:'dashboard', component: DashboardComponent},
     {path:'aboutus', component: AboutusComponent}, 
     {path:'contactus', component: ContactUsComponent},
-    {path:'bucketlist', component: BucketListComponent},
-    {path:'travel', component: TravelComponent}
-
+    {path:'**', component:LoginComponent},
 ];
+
+
+NgModule({
+    imports:[CommonModule, RouterOutlet, RouterLink, RouterLinkActive]
+})
